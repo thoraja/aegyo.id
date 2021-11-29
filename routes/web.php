f11d27caa2dcd3ef8a6db282f4db00865ac5a360
thoraja <artix.mod@gmail.com>
2021-11-29 21:24:18 +0700

    Implements laravel auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Content\ArticleController as APIArticleController;
+use App\Http\Controllers\API\Content\CategoryController as APICategoryController;
+use App\Http\Controllers\Content\ArticleController;
+use App\Http\Controllers\Content\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
